288552c4717aa7f24d8a8a0ed7349638cffc0873
PawelSuchta <altar8@wp.pl>
2019-11-25 16:36:35 +0100

    change ports

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
  
-server.listen(3000, function () {
+server.listen(process.env.PORT || 3000, function () {
   console.log(`Listening on ${server.address().port}`);
 });
